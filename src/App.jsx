0e6d91ac789cd9309fb3ab3af4d38492b8ac01ab
Juan <juancruzlambrechts@negsoluciones.com>
2023-04-18 09:59:55 -0300

    added project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { lazy, Suspense } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence, motion } from 'framer-motion';
+import User from './components/views/User/User'
+import Skill from './components/views/Skill/Skill'
 import './App.css'
 
+const Error404 = lazy(() => import('./components/views/Error404/Error404'));
+
+const pageTransition = {
+  in: {
+    opacity: 1
+  },
+  out: {
+    opacity: 0,
+  },
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const location = useLocation()
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <>
+      <AnimatePresence>
+        <Routes location={location} key={location.pathname}>
+          <Route
+            path='/'
+            element={
+              <motion.div className='page' initial='out' animate='in' exit='out' variants={pageTransition}>
+                <User />
+              </motion.div>
+            } />
+          <Route
+            path='/:skill'
+            element={
+              <motion.div className='page' initial='out' animate='in' exit='out' variants={pageTransition}>
+                <Skill />
+              </motion.div>
+            } />
+          <Route
+            path='*'
+            element={
+              <motion.div className='page' initial='out' animate='in' exit='out' variants={pageTransition}>
+                <Suspense fallback={<>...</>}>
+                  <Error404 />
+                </Suspense>
+              </motion.div>
+            } />
+        </Routes>
+      </AnimatePresence>
+    </>
   )
 }
 
